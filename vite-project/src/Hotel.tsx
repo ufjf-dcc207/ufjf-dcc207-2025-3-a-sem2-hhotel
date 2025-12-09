047c72628d6827fc3f66e8e0c6008aae65295542
Danrleygd <danrley.dias@estudante.ufjf.br>
2025-12-09 01:58:30 -0300

    adiciona mais hospedes e logica para tal

--- vite-project/src/Hotel.tsx
+++ vite-project/src/Hotel.tsx
@@ -1,85 +1,113 @@
 import { useState } from "react";
 import "./hotel.css";
-import {Atributo} from "./Atributo";
-
+import data from "./data.json";
+import { Atributo } from "./Atributo";
 
 export type HospedeType = {
   nome: string;
   bondade: number;
   maldade: number;
   situacao: string;
+  imagemNeutra: string;
+  imagemRedimido: string;
+  imagemSoberano: string;
 };
 
 export default function Hotel() {
-  const ImagemNeutra: string = "https://static.zerochan.net/Sir.Pentious.1024.2822103.webp";
-  const ImagemRedimido: string = "https://i.pinimg.com/736x/1c/4b/fe/1c4bfe36dba569e3a7d51af4f61c117a.jpg";
-  const ImagemSoberano: string = "https://i.pinimg.com/1200x/4c/d5/7d/4cd57d3ecc22188c53bb284118199a94.jpg";  
+  const [hospedes, setHospedes] = useState<HospedeType[]>(data);
+  const [index, setIndex] = useState(0);
+  const atual = hospedes[index];
 
-  const [hospedes, setHospedes] = useState<HospedeType>({
-    nome: "Sr. Pentious",
-    bondade: 3,
-    maldade: 2,
-    situacao: "Neutro"
-  });
+  function PróximoHospede() {
+    const próximoIndex = (index + 1) % hospedes.length;
+    setIndex(próximoIndex);
+  }
 
-
+  function AnteriorHospede() {
+    const anteriorIndex = (index - 1 + hospedes.length) % hospedes.length;
+    setIndex(anteriorIndex);
+  }
 
   function BoasAções() {
-    // garantir que os valores fiquem entre 0 e 5
-    console.log("Executando BoasAções");
-    const novoBondade = Math.min(5, hospedes.bondade + 1);
-    const novoMaldade = Math.max(0, hospedes.maldade - 1);
-    
-    // se nada mudar, não atualiza
-    if (novoBondade === hospedes.bondade && novoMaldade === hospedes.maldade) return;
-    const novaSituacao = (novoBondade === 5) ? "Redimido" : (novoMaldade === 5) ? "Soberano" : "Neutro";
-    setHospedes({
-      ...hospedes,
+    const novoBondade = Math.min(5, atual.bondade + 1);
+    const novoMaldade = Math.max(0, atual.maldade - 1);
+
+    if (novoBondade === atual.bondade && novoMaldade === atual.maldade) return;
+
+    const novaSituacao =
+      novoBondade === 5
+        ? "Redimido"
+        : novoMaldade === 5
+        ? "Soberano"
+        : "Neutro";
+
+    const atualiza = [...hospedes];
+    atualiza[index] = {
+      ...atualiza[index],
       situacao: novaSituacao,
       bondade: novoBondade,
       maldade: novoMaldade,
-    });
+    };
+
+    setHospedes(atualiza);
   }
 
   function MasAções() {
-    // garantir que os valores fiquem entre 0 e 5
-    console.log("Executando MasAções");
-    const novoMaldade = Math.min(5, hospedes.maldade + 1);
-    const novoBondade = Math.max(0, hospedes.bondade - 1);
-    if (novoMaldade === hospedes.maldade && novoBondade === hospedes.bondade) return;
-    const novaSituacao = (novoMaldade === 5) ? "Soberano" : (novoBondade === 5) ? "Redimido" : "Neutro";
-    setHospedes({
-      ...hospedes,
+    const novoMaldade = Math.min(5, atual.maldade + 1);
+    const novoBondade = Math.max(0, atual.bondade - 1);
+
+    if (novoMaldade === atual.maldade && novoBondade === atual.bondade) return;
+
+    const novaSituacao =
+      novoMaldade === 5
+        ? "Soberano"
+        : novoBondade === 5
+        ? "Redimido"
+        : "Neutro";
+
+    const atualiza = [...hospedes];
+    atualiza[index] = {
+      ...atualiza[index],
       situacao: novaSituacao,
       bondade: novoBondade,
       maldade: novoMaldade,
-    });
+    };
+
+    setHospedes(atualiza);
   }
 
   function ImagemSituacao() {
-    if (hospedes.situacao === "Redimido") {
-      return ImagemRedimido;
-    } else if (hospedes.situacao === "Soberano") {
-      return ImagemSoberano;
+    if (atual.situacao === "Redimido") {
+      return atual.imagemRedimido;
+    } else if (atual.situacao === "Soberano") {
+      return atual.imagemSoberano;
     } else {
-      return ImagemNeutra;
+      return atual.imagemNeutra;
     }
   }
 
-
   return (
     <div className="hotel">
-      <div className="situacao">{hospedes.situacao}</div>
-      <img src={ImagemSituacao()} style={{ width: '200px', border: '1px solid red' }} />
+      <div className="situacao">{atual.situacao}</div>
+      <img
+        src={ImagemSituacao()}
+        style={{ width: "200px", border: "1px solid red" }}
+      />
       <div className="hospedes">
-        <div className="nome">{hospedes.nome}</div>
-        <Atributo icone="😇" valor={hospedes.bondade} />
-        <Atributo icone="😈" valor={hospedes.maldade} />
+        <div className="nome">{atual.nome}</div>
+        <Atributo icone="😇" valor={atual.bondade} />
+        <Atributo icone="😈" valor={atual.maldade} />
       </div>
       <div className="acoes">
         <button onClick={BoasAções}>Ajudar uma velinha</button>
+        <button onClick={BoasAções}>Enfrentar seu pecado</button>
         <button onClick={MasAções}>Roubar um doce de uma criança</button>
         <button onClick={MasAções}>Sabotar alguém</button>
+
+        <div className="navegacao">
+          <button onClick={AnteriorHospede}>◀ Anterior</button>
+          <button onClick={PróximoHospede}>Próximo ▶</button>
+        </div>
       </div>
     </div>
   );
